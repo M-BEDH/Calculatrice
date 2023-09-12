15a49fb9281e0530f4f61a5d49453770f2edc6ba
M-BEDH <mel.b.ms@pm.me>
2023-09-12 13:29:26 +0200

    deploy app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import React, { useState } from 'react';
 import Btn from './components/Btn/Btn';
 import './App.css';
 
 
 function App() {
-   const handleButtonClick = (value) => {
-    // Cette fonction sera appelée lorsque vous cliquez sur un bouton avec la valeur "value".
+  const [expression, setExpression] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleButtonClick = (value) => {
+    // Cette fonction sera appelée lors du click sur un bouton avec la valeur "value".
     console.log('Button clicked with value:', value);
-  };
+    if (value === '=') {
+      try {
+  
+          const evalResult = evaluateExpression(expression);//evalue l'expression
+        setResult(evalResult.toString()); //Met à jour le résultat
+        setExpression(''); //Réinitilise l'expression
+      } catch (error) {
+        setResult('Erreur');
+      }
+    } else {
+       setExpression((prevExpression) => prevExpression + value);
+    }
+   };
+
+   const evaluateExpression = (expr) => {
+     // Implémentez votre propre logique d'évaluation ici
+    // Vous pouvez utiliser des bibliothèques tierces pour vous aider dans cette tâche
+    // Assurez-vous de gérer correctement les erreurs et les opérations mathématiques
+
+    // Exemple simple en utilisant eval (ceci est pour démonstration seulement)
+    return eval(expr);
+     
+     
+   };
+
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -14,7 +43,23 @@ function App() {
          </header>
       
         <div className='App-body'>
-         <Btn onClick={handleButtonClick} /> 
+   
+        <input
+          type='text'
+          className='expression-input'
+          value={expression}
+          readOnly={true}
+        />
+         <Btn onClick={handleButtonClick} />
+      
+          <input
+            type='text'
+            className='resultat-input'
+            value={result}
+            readOnly={true}
+          />     
+        
+        
         </div>
     </div>
   );
--- src/components/Btn/Btn.js
+++ src/components/Btn/Btn.js
@@ -1,24 +1,15 @@
 import './Btn.css';
 
 function Btn({ onClick }) {
-  // Créez un tableau de chiffres de 0 à 9
-  const chiffres = [...Array(10).keys()];
+  const chiffresEtOperateurs = ['C', 'CE','/', '*', '-', '+', '.','%', '**', ...Array(10).keys(), '='];
 
   return (
     <div className='App-body'>
-      <button className='btn' onClick={() => onClick('C')}>C</button>
-      <button className='btn' onClick={() => onClick('CE')}>CE</button>
-      <button className='btn' onClick={() => onClick('v2')}>v2</button>
-      <button className='btn' onClick={() => onClick('/')}>/</button>
-      <button className='btn' onClick={() => onClick('x')}>x</button>
-      <button className='btn' onClick={() => onClick('-')}>-</button>
-      <button className='btn' onClick={() => onClick('+')}>+</button>
-      <button className='btn' onClick={() => onClick('.')}>.</button>
-    
-      {chiffres.map((chiffre) => (
-        <button className='btn' key={chiffre} onClick={() => onClick(chiffre)}>{chiffre}</button>
+      {chiffresEtOperateurs.map((value) => (
+        <button className='btn' key={value} onClick={() => onClick(value)}>
+          {value}
+        </button>
       ))}
-      <button className='btn' onClick={() => onClick('=')}>=</button>
     </div>
   );
 }
